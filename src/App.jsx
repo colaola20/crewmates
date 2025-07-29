b295a69c81b3628c701d80dfbfbd1c04e5895096
colaola20 <soryo@farmingdale.edu>
2025-07-29 13:57:41 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="main-page">
+        <div className="nav-bar">
+
+        </div>
+        <div className="main-context">
+          <h1>Main part</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
